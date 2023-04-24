f41a85f34b9ae3ce86cfae3e0030d10cc9d390d2
manasabathini <33703157+manasabathini@users.noreply.github.com>
2023-04-24 05:32:40 +0000

    4th day of coding

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!")
+
+yourName = input("What is your name?")
+enemyName= input("What is your worst enemy’s name?")
+superPower=input("What is your superpower?")
+address = input("Where do you live?")
+favoriteFood = input("What is your favorite food?")
+print()
+
+print("Hello", yourName,"! Your ability to", superPower, "will make sure you never have to look at", enemyName, "again. Go eat", favoriteFood, "as you walk down the streets of", address, "and use", superPower, "for good and not evil!")
